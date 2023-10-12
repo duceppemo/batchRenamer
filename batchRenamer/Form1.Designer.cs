acf011aaff252982adc38167fe67f10128b604d9
Marc-Olivier Duceppe <17859096+duceppemo@users.noreply.github.com>
2023-10-12 10:33:15 -0400

    Add project files.

---
+++ batchRenamer/Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace batchRenamer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.buttonOpen = new System.Windows.Forms.Button();
+            this.buttonRename = new System.Windows.Forms.Button();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.toolTip2 = new System.Windows.Forms.ToolTip(this.components);
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.buttonValidate = new System.Windows.Forms.Button();
+            this.labelStatus = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Translation Table";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 113);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(323, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "List of files that will be renamed (imported from the translation table):";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // buttonOpen
+            // 
+            this.buttonOpen.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonOpen.Location = new System.Drawing.Point(480, 11);
+            this.buttonOpen.Name = "buttonOpen";
+            this.buttonOpen.Size = new System.Drawing.Size(75, 23);
+            this.buttonOpen.TabIndex = 3;
+            this.buttonOpen.Text = "Open";
+            this.buttonOpen.UseVisualStyleBackColor = true;
+            this.buttonOpen.Click += new System.EventHandler(this.buttonOpen_Click);
+            // 
+            // buttonRename
+            // 
+            this.buttonRename.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonRename.Enabled = false;
+            this.buttonRename.Location = new System.Drawing.Point(480, 79);
+            this.buttonRename.Name = "buttonRename";
+            this.buttonRename.Size = new System.Drawing.Size(75, 22);
+            this.buttonRename.TabIndex = 4;
+            this.buttonRename.Text = "Rename";
+            this.buttonRename.UseVisualStyleBackColor = true;
+            this.buttonRename.Click += new System.EventHandler(this.buttonRename_Click);
+            // 
+            // toolTip1
+            // 
+            this.toolTip1.AutoPopDelay = 10000;
+            this.toolTip1.InitialDelay = 500;
+            this.toolTip1.ReshowDelay = 100;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 136);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(543, 150);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Input Name";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            this.Column1.Width = 87;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Output Name";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            this.Column2.Width = 95;
+            // 
+            // textBox1
+            // 
+            this.textBox1.AllowDrop = true;
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(125, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(349, 20);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.DragDrop += new System.Windows.Forms.DragEventHandler(this.textBox1_DragDrop);
+            this.textBox1.DragEnter += new System.Windows.Forms.DragEventHandler(this.textBox1_DragEnter);
+            this.textBox1.MouseHover += new System.EventHandler(this.textBox1_MouseHover);
+            // 
+            // buttonValidate
+            // 
+            this.buttonValidate.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonValidate.Enabled = false;
+            this.buttonValidate.Location = new System.Drawing.Point(480, 45);
+            this.buttonValidate.Name = "buttonValidate";
+            this.buttonValidate.Size = new System.Drawing.Size(75, 23);
+            this.buttonValidate.TabIndex = 7;
+            this.buttonValidate.Text = "Validate";
+            this.buttonValidate.UseVisualStyleBackColor = true;
+            this.buttonValidate.Click += new System.EventHandler(this.buttonValidate_Click);
+            // 
+            // labelStatus
+            // 
+            this.labelStatus.AutoSize = true;
+            this.labelStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelStatus.Location = new System.Drawing.Point(120, 57);
+            this.labelStatus.Name = "labelStatus";
+            this.labelStatus.Size = new System.Drawing.Size(278, 25);
+            this.labelStatus.TabIndex = 8;
+            this.labelStatus.Text = "Translation table is valid!";
+            this.labelStatus.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(569, 298);
+            this.Controls.Add(this.labelStatus);
+            this.Controls.Add(this.buttonValidate);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.buttonRename);
+            this.Controls.Add(this.buttonOpen);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(585, 336);
+            this.Name = "Form1";
+            this.Text = "batchRenamer v0.2.2";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button buttonOpen;
+        private System.Windows.Forms.Button buttonRename;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.ToolTip toolTip2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button buttonValidate;
+        private System.Windows.Forms.Label labelStatus;
+    }
+}
+
---
+++ batchRenamer/Form1.cs
@@ -0,0 +1,364 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace batchRenamer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //For debug
+            //textBox1.Text = @"C:\Users\duceppem\Desktop\testRename.txt";
+            openFileDialog1.Filter = "Tab-separated Text File (*.txt, *.tsv)|*.txt;*.tsv|All Files (*.*)|*.*";
+            
+            textBox1.AllowDrop = true;
+            textBox1.DragEnter += new DragEventHandler(textBox1_DragEnter);
+            textBox1.DragDrop += new DragEventHandler(textBox1_DragDrop);
+        }
+
+        private void buttonOpen_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            //Display file name in textbox
+            textBox1.Text = openFileDialog1.FileName;
+
+            //Validate button now available
+            buttonValidate.Enabled = true;
+        }
+
+        private void textBox1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Copy;
+            }
+        }
+
+        private void textBox1_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+            if (files != null && files.Length != 0)
+            {
+                textBox1.Text = files[0];
+                clearDataGridView();
+            }
+
+            //Validate button now available
+            buttonValidate.Enabled = true;
+        }
+
+        private void clearDataGridView()
+        {
+            dataGridView1.Rows.Clear();
+            dataGridView1.Refresh();
+        }
+
+        //Remove leading and trailing double quotes is present (illegal character)
+        private string trimBeginEnd(Char t, string s)
+        {
+            if (s.StartsWith(t.ToString()) == true && s.EndsWith(t.ToString()) == true)
+            {
+                //s = s.TrimStart(new Char[] { t }).TrimEnd(new Char[] { t });
+                return s.TrimStart(new Char[] { t }).TrimEnd(new Char[] { t });
+            }
+            else
+            {
+                return s;
+            }
+        }
+
+        private bool isDirectory(string s)
+        {
+            bool isIt = false;
+            if (System.IO.Directory.Exists(s))
+            {
+                isIt = true;
+                MessageBox.Show("The following entry from the translation table is a directory:" + '\n' + '\n' + s,
+                    "Invalid Translation Table", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            return isIt;
+        }
+
+        private void buttonValidate_Click(object sender, EventArgs e)
+        {
+            //Make sure button2 label and datagridview reverts back to original in case multiple renaming actions occur in one session
+            buttonRename.Text = "Rename";
+            clearDataGridView();
+
+            //Show content of translation table in the dataGridView
+            string[] lineArray = new string[2];
+
+            try
+            {
+
+                //Check is translation table file is empty
+                if (new System.IO.FileInfo(textBox1.Text).Length == 0)
+                {
+                    MessageBox.Show("Translation table file is empty!",
+                                        "Invalid Translation Table", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+                    return;
+                }
+
+                //Open file stream
+                System.IO.Stream fileStream = System.IO.File.OpenRead(textBox1.Text);
+
+                int lineNumber = 0;
+                string line;
+                if (fileStream != null)
+                {
+                    using (System.IO.StreamReader sr = new System.IO.StreamReader(fileStream))
+                    {
+                        while ((line = sr.ReadLine()) != null)
+                        {
+                            lineNumber += 1;
+
+                            //string line = sr.ReadLine();
+                            if (!string.IsNullOrEmpty(line)) //if line not empty
+                            {
+                                //Split 
+                                string[] fields = line.Split('\t');
+
+                                //Check if is there is only two fields separated by a tab
+                                if (fields.Length != 2)
+                                {
+                                    MessageBox.Show("Translation table must be a tab-separated file with 2 columns." + "\n"
+                                        + "Problem found at line " + lineNumber,
+                                        "Invalid Translation Table", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+                                    clearDataGridView();
+                                    return; //Stop further execution
+                                }
+
+                                string input = fields[0];
+                                string output = fields[1];
+
+                                //Remove leading and trailing double quotes if present (illegal character)
+                                input = trimBeginEnd('"', input);
+                                output = trimBeginEnd('"', output);
+
+                                
+                                //Check if translation table format is valid
+
+
+                                
+
+                                //Check if input is a directory
+                                if (isDirectory(input) == true || isDirectory(output) == true)
+                                {
+                                    clearDataGridView();
+                                    return;
+                                }
+
+                                //Check if input file exists
+                                string inputDirectoryName;
+                                string inputFileName;
+
+                                if (System.IO.File.Exists(input) == false)
+                                {
+                                    MessageBox.Show("The following file at line " + lineNumber + " from the translation table does not exist:" + '\n' + '\n' + input,
+                                        "Invalid Translation Table", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+                                    //Clear datagridview
+                                    clearDataGridView();
+                                    return; //Stop further execution
+                                }
+                                else
+                                {
+                                    inputDirectoryName = System.IO.Path.GetDirectoryName(input);
+                                    inputFileName = System.IO.Path.GetFileName(input);
+                                }
+
+                                //Check for illegal characters
+                                string outputDirectoryName;
+                                string outputFileName;
+
+                                try
+                                {
+                                    outputDirectoryName = System.IO.Path.GetDirectoryName(output);
+                                }
+                                catch
+                                {
+                                    MessageBox.Show("The following ouput path at line " + lineNumber + " has illegal characters:" + '\n' + '\n' + output,
+                                        "Invalid Translation Table", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                                    
+                                    clearDataGridView();
+                                    return;
+                                }
+                                
+                                //File name
+                                outputFileName = output.Substring(outputDirectoryName.Length + 1, output.Length - outputDirectoryName.Length - 1);
+                                
+                                char[] invalidPathChars = System.IO.Path.GetInvalidPathChars();
+                                char[] invalidFileChars = System.IO.Path.GetInvalidFileNameChars();
+
+                                foreach(char c in invalidFileChars)
+                                {
+                                    if (outputFileName.Contains(c))
+                                    {
+                                        MessageBox.Show("The following output file name at line " + lineNumber + " has illegal charaters:" + '\n' + '\n' + output,
+                                       "Invalid Translation Table", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+                                        clearDataGridView();
+                                        return;
+                                    }
+                                }
+
+                                //Check if path of original and renamed file is the same
+                                if (inputDirectoryName != outputDirectoryName)
+                                {
+                                    MessageBox.Show("Different input and output paths at line " + lineNumber + ":" + '\n' + '\n' + line + '\n' + '\n' +
+                                    "This application can only rename files in place.",
+                                       "Invalid Translation Table", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+                                    clearDataGridView();
+                                    return;
+                                }
+
+
+                                //Check that original file and renamed file names are different
+                                if (outputFileName == inputFileName)
+                                {
+                                    MessageBox.Show("Input and output file name identical at line " + lineNumber + ":" + '\n' + '\n' + line,
+                                   "Invalid Translation Table", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+                                    clearDataGridView();
+                                    return;
+                                }
+                               
+
+                                //Check if the output file has non-alphanumerical characters
+                                Regex rgx = new Regex("[^a-zA-Z0-9-_.]");
+
+                                if (rgx.IsMatch(outputFileName) == true)
+                                {
+                                    MessageBox.Show("Please only use alphanumerical characters in output name at line " + lineNumber + ":" + '\n' + '\n' + outputFileName,
+                                       "Invalid Translation Table", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+                                    clearDataGridView();
+                                    return;
+                                }
+                                
+                                //Populate the datagridview
+                                dataGridView1.Rows.Add(input, output);
+
+                                //For debug
+                                // Read the first line from the file and write it the textbox.
+                                //richTextBox1.Text = line;
+                            }
+                        }
+                    }
+                }
+
+                //Close file stream
+                fileStream.Close();
+
+                //Check if duplicates in both input and output names
+                for (int currentRow = 0; currentRow < dataGridView1.Rows.Count - 1; currentRow++)
+                {
+                    DataGridViewRow rowToCompare = dataGridView1.Rows[currentRow];
+
+                    for (int otherRow = currentRow + 1; otherRow < dataGridView1.Rows.Count; otherRow++)
+                    {
+                        DataGridViewRow row = dataGridView1.Rows[otherRow];
+
+                        bool duplicateRow = true;
+
+                        for (int cellIndex = 0; cellIndex < row.Cells.Count; cellIndex++)
+                        {
+                            if (!rowToCompare.Cells[cellIndex].Value.Equals(row.Cells[cellIndex].Value))
+                            {
+                                duplicateRow = false;
+                                break;
+                            }
+                        }
+
+                        if (duplicateRow == true)
+                        {
+                            MessageBox.Show("Line " + lineNumber + " contains a duplicate:" + '\n' + '\n' + 
+                                dataGridView1.Rows[currentRow].Cells[0].Value.ToString() + '\t' +
+                                dataGridView1.Rows[currentRow].Cells[1].Value.ToString(),
+                                "Invalid Translation Table", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+                            //Clear datagridview
+                            clearDataGridView();
+
+                            return; //Stop further execution
+                        }
+                    }
+                }
+
+                //Enable the Rename button and show validation message
+                labelStatus.Visible = true;
+                buttonRename.Enabled = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", 
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void buttonRename_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                for (int i = 0; i < dataGridView1.Rows.Count; i++)
+                {
+                    //Get the old and new file names from the datagriview table
+                    string oldName = dataGridView1.Rows[i].Cells[0].Value.ToString();
+                    string newName = dataGridView1.Rows[i].Cells[1].Value.ToString();
+
+                    //Replace file names
+                    System.IO.File.Move(oldName, newName);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", 
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+            //Show complete message and diable the rename button to prevent error on re-click
+            //Check if last file to rename exists and is not locked
+            string lastFile = dataGridView1.Rows[dataGridView1.Rows.Count - 1].Cells[1].Value.ToString();
+
+            while (System.IO.File.Exists(lastFile) == false)
+            {
+                //wait
+            }
+
+            //Completion message
+            labelStatus.Text = "File renaming completed!";
+            buttonRename.Enabled = false;
+        }
+
+        private void textBox1_MouseHover(object sender, EventArgs e)
+        {
+            //Build tip message
+            string tipText1 = "A tab-separated text file with two columns:" + '\n' + '\n' +
+                @"C:\Users\username\Desktop\file_to_rename.txt" + '\t' + @"C:\Users\username\Desktop\renamed_file.txt" + '\n' + '\n' +
+            "Note that paths must be identical. Just the file names are changing.";
+            //Assign tip message to textbox
+            toolTip1.SetToolTip(textBox1, tipText1);
+        }
+    }
+}
